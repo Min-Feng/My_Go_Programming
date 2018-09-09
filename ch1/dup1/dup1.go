be0227f734bc90683d6c189cadc5a6a6003e5f7f
CaesarTsai <x24libra@hotmail.com>
2018-09-09 10:13:54 +0800

    ch1-dup1

---
+++ ch1/dup1/dup1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	for key, val := range counts {
+		fmt.Printf("%s\t%d\n", key, val)
+	}
+}
