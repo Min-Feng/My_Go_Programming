a1067888f64cf736017637a6c8dd94063a92990d
CaesarTsai <x24libra@hotmail.com>
2018-09-10 20:25:45 +0800

    ch1-server2- 注意favicon.ico

--- ch1/server2/server2.go
+++ ch1/server2/server2.go
@@ -19,11 +19,15 @@ func hander(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	count++
 	mu.Unlock()
+	//若以瀏覽器開啟 來產生request
+	//需要注意 /favicon.ico 的請求,會造成count多計算一次
+	fmt.Printf("count=%d URL.Path=%q \n", count, r.URL.Path)
 	fmt.Fprintf(w, "count=%d URL.Path=%q \n", count, r.URL.Path)
 }
 
 func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
+	fmt.Printf("count=%d URL.Path=%q \n", count, r.URL.Path)
 	fmt.Fprintf(w, "count=%d\n", count)
 	mu.Unlock()
 }
